84bc4088dce783124a6b9f3bae6e71e1b0a413d5
My_computer <My_computer@DESKTOP-CHE6UO4>
2023-12-08 23:15:55 +0900

    로그인/회원가입 구현중

---
+++ docent/src/main/java/com/fred/docent/domain/PasswordCheck.java
@@ -0,0 +1,16 @@
+package com.fred.docent.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter @Setter @ToString
+public class PasswordCheck {
+	
+	private String email;
+	private String password;
+}
