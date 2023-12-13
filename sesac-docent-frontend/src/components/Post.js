287fb75c662efc62c5e9df43bfef8db4200d9f60
lake041 <lake041@korea.ac.kr>
2023-12-13 10:43:52 +0900

    Feat: Reply 컴포넌트

--- sesac-docent-frontend/src/components/Post.js
+++ sesac-docent-frontend/src/components/Post.js
@@ -2,9 +2,12 @@ import { useEffect, useState } from "react";
 import { useLocation, useParams } from "react-router-dom";
 import sanitize from "dompurify";
 import api from "apis/api";
+import { DUMMY_REPLY } from "./DUMMY_REPLY";
+import { Reply } from "./Reply";
 
 export const Post = () => {
   const [data, setData] = useState();
+  const [sortType, setSortType] = useState("popular");
   const params = useParams();
   const postId = params.postId;
   const location = useLocation();
@@ -19,9 +22,11 @@ export const Post = () => {
   // }, [categoryENG, postId]);
 
   return (
-    <div className="w-full h-full flex justify-center items-start p-5 my-20">
-      <div className="w-full max-w-[1300px] h-full px-10 py-5 rounded-xl bg-white flex flex-col justify-start items-center gap-6">
-        <div className="quill-container w-full flex flex-col gap-4">
+    <div className="w-full h-full flex flex-col justify-center items-center p-5 my-20 gap-4">
+      {/* post 영역 */}
+      <div className="w-full max-w-[1000px] px-10 py-5 rounded-xl bg-white flex flex-col justify-center items-center gap-6">
+        {/* 제목영역 */}
+        <div className="w-full flex flex-col gap-4">
           <div className="w-full flex gap-2">
             <label
               htmlFor="title"
@@ -34,8 +39,10 @@ export const Post = () => {
             </p>
           </div>
         </div>
+        {/* 본문영역 */}
         <div className="w-full border border-black p-5">
-          {data?.content && (
+          본문 영역
+          {/* {data?.content && (
             <div
               style={{
                 width: "60vw",
@@ -45,9 +52,10 @@ export const Post = () => {
                 __html: sanitize(String(data?.content)),
               }}
             />
-          )}
+          )} */}
         </div>
-        <div className="mt-2 max-w-[1300px] w-full flex justify-end gap-2">
+        {/* 버튼영역 (글쓴이만 볼 수 있도록) */}
+        <div className="mt-2 w-full flex justify-end gap-2">
           <button
             style={{ marginTop: "50px" }}
             className="w-fit h-fit px-4 py-2 border border-black text-lg font-bold hover:bg-black hover:text-white transition"
@@ -60,6 +68,66 @@ export const Post = () => {
           >
             등록
           </button>
+        </div>
+      </div>
+      {/* reply 영역 */}
+      <div className="w-full max-w-[1000px] flex px-10 flex-col gap-4">
+        <div className="flex justify-end items-center w-full">
+          <button
+            onClick={() => setSortType("popular")}
+            className={`p-2  ${
+              sortType === "popular"
+                ? "text-black font-medium"
+                : "text-zinc-400 font-normal"
+            }`}
+          >
+            인기순
+          </button>
+          <p className="select-none flex items-center">┃</p>
+          <button
+            onClick={() => setSortType("recent")}
+            className={`p-2  ${
+              sortType === "recent"
+                ? "text-black font-medium"
+                : "text-zinc-400 font-normal"
+            }`}
+          >
+            최신순
+          </button>
+        </div>
+        <div className="w-full flex flex-col gap-8">
+          {DUMMY_REPLY.map((reply) => (
+            <Reply
+              key={reply.replyId}
+              replyId={reply.replyId}
+              username={reply.username}
+              content={reply.content}
+              date={reply.date}
+              like={true}
+              likeCount="72"
+              myLike={true}
+            />
+          ))}
+        </div>
+      </div>
+      {/* reply write 영역 */}
+      <div className="w-full max-w-[1000px] px-10 py-5 flex justify-center">
+        <div className="w-full max-w-[1000px] p-5 border border-black flex flex-col gap-4">
+          <div className="flex justify-between items-center">
+            <label htmlFor="newReply" className="text-lg font-semibold pl-2">
+              새 댓글
+            </label>
+            <button className="w-fit h-fit px-4 py-2 border border-black text-lg font-bold hover:bg-black hover:text-white transition">
+              입력
+            </button>
+          </div>
+          <textarea
+            name="newReply"
+            className="w-full p-5 h-40 border border-zinc-300"
+            //   value="하이요"
+            placeholder="댓글을 작성하려면 로그인해주세요."
+          ></textarea>
+          <div className="flex justify-end"></div>
         </div>
       </div>
     </div>
--- sesac-docent-frontend/src/components/Reply.js
+++ sesac-docent-frontend/src/components/Reply.js
@@ -2,8 +2,17 @@ import { useEffect, useState } from "react";
 import { useLocation, useParams } from "react-router-dom";
 import sanitize from "dompurify";
 import api from "apis/api";
+import { Heart, HeartOff } from "lucide-react";
 
-export const Reply = () => {
+export const Reply = ({
+  replyId,
+  username,
+  content,
+  date,
+  like,
+  likeCount,
+  myLike,
+}) => {
   const [data, setData] = useState();
   const params = useParams();
   const postId = params.postId;
@@ -11,36 +20,42 @@ export const Reply = () => {
   const categoryENG = location.state && location.state.categoryENG;
   const categoryKOR = location.state && location.state.categoryKOR;
 
-  // useEffect(() => {
-  //   (async () => {
-  //     const response = await api.get(`/${categoryENG}/posts/${postId}`);
-  //     setData(response.data);
-  //   })();
-  // }, [categoryENG, postId]);
+  const heartClickHandler = () => {
+    console.log("heart clicked");
+  };
 
   return (
-    <div className="w-full h-full flex justify-center items-start p-5 my-20">
-      <div className="w-full max-w-[1300px] h-full px-10 py-5 rounded-xl bg-white flex flex-col justify-start items-center gap-6">
-        <div className="w-full border border-black p-5">
-          {data?.content && (
-            <div
-              style={{
-                width: "60vw",
-                whiteSpace: "normal",
-              }}
-              dangerouslySetInnerHTML={{
-                __html: sanitize(String(data?.content)),
-              }}
-            />
-          )}
-        </div>
-        <div className="mt-2 max-w-[1300px] w-full flex justify-end gap-2">
-          <button
-            style={{ marginTop: "50px" }}
-            className="w-fit h-fit px-4 py-2 border border-black text-lg font-bold hover:bg-black hover:text-white transition"
-          >
-            등록
-          </button>
+    <div className="w-full h-full flex justify-center items-start">
+      <div className="w-full max-w-[1000px] h-full rounded-xl bg-white flex flex-col justify-start items-center gap-6">
+        <div className="w-full border border-black p-5 flex flex-col gap-4">
+          <div className="flex justify-between items-center">
+            {/* 댓글 헤더 */}
+            <div className="flex flex-col justify-center gap-2 text-sm">
+              <div className="flex items-center gap-3">
+                <div className="w-fit h-fit p-1 px-2 border border-black text-base hover:bg-black hover:text-white transition">
+                  <p>댓글 1</p>
+                </div>
+                <p className="text-xl font-semibold">{username}</p>
+              </div>
+              <p className="text-zinc-500">{date}</p>
+            </div>
+            <button
+              className="flex gap-2 items-center"
+              onClick={heartClickHandler}
+            >
+              {myLike ? (
+                <Heart
+                  size={30}
+                  fill="rgb(239, 68, 68)"
+                  color="rgb(239, 68, 68)"
+                />
+              ) : (
+                <Heart size={30} />
+              )}
+              <p className="text-lg">{likeCount}</p>
+            </button>
+          </div>
+          <p className="text-lg">{content}</p>
         </div>
       </div>
     </div>
