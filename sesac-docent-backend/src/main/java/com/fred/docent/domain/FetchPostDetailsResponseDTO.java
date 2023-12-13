69128c73386c560194b801dec685263e557e9de9
osy <osy9757@gmail.com>
2023-12-14 04:15:46 +0900

    [Backend] Feat: Detail 출력 수정, galleries, exhibitions authors works list출력 pl/sql 제작

--- sesac-docent-backend/src/main/java/com/fred/docent/domain/FetchPostDetailsResponseDTO.java
+++ sesac-docent-backend/src/main/java/com/fred/docent/domain/FetchPostDetailsResponseDTO.java
@@ -7,29 +7,29 @@ import lombok.Data;
 public class FetchPostDetailsResponseDTO {
 
     // 공통 필드
-    private Long postId;
-    private String userName;
-    private Integer postValid;
-    private String postTitle;
-    private String postContent;
-    private Integer postViews;
-    private Date postCreatedAt;
-    private Date postUpdatedAt;
+    private Long post_id;
+    private String user_name;
+    private Integer post_valid;
+    private String post_title;
+    private String post_content;
+    private Integer post_views;
+    private Date post_created_at;
+    private Date post_updated_at;
 
     // 카테고리 2 전용 필드
-    private String exhibitionName;
-    private String exhibitionDescription;
-    private Date exhibitionStartDate;
-    private Date exhibitionEndDate;
-    private Integer exhibitionValid;
-    private String exhibitionUrl;
-    private Integer postLikes;
+    private String exhibition_name;
+    private String exhibition_description;
+    private Date exhibition_start_date;
+    private Date exhibition_end_date;
+    private Integer exhibition_valid;
+    private String exhibition_url;
+    private Integer post_likes;
 
     // 카테고리 3 전용 필드
-    private String replyUserName;
-    private Integer replyPostValid;
-    private String replyPostTitle;
-    private String replyPostContent;
-    private Date replyPostCreatedAt;
+    private String reply_user_name;
+    private Integer reply_post_valid;
+    private String reply_post_title;
+    private String reply_post_content;
+    private Date reply_post_created_at;
 
 }
