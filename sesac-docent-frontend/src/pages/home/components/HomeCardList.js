3af1ed87a8d6303c3defcaed5fb44c0706e8eb9c
osy9757 <osy9757@gmail.com>
2023-12-14 23:56:03 +0900

    [Backend] Error: fetchArtCollection 수정

--- sesac-docent-frontend/src/pages/home/components/HomeCardList.js
+++ sesac-docent-frontend/src/pages/home/components/HomeCardList.js
@@ -1,7 +1,28 @@
 import { HomeCard } from "./HomeCard";
 
 export const HomeCardList = ({ cards, dummyCards }) => {
-  
+  const propertyMappings = {
+    galleries: {
+      imgProperty: 'gallery_img',
+      titleProperty: 'gallery_name',
+      descProperty: 'gallery_describe',
+    },
+    exhibitions: {
+      imgProperty: 'gallery_img',
+      titleProperty: 'gallery_title',
+      descProperty: 'gallery_describe',
+    },
+    authors: {
+      imgProperty: 'gallery_img',
+      titleProperty: 'gallery_title',
+      descProperty: 'gallery_describe',
+    },
+    works: {
+      imgProperty: 'gallery_img',
+      titleProperty: 'gallery_title',
+      descProperty: 'gallery_describe',
+    },
+  }
   return (
     <div className="flex justify-between">
       {dummyCards.map((card, index) => (
